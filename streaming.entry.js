f2d5c5f4c41e6486e4924af44e5c12f1fa0d2b0e
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 12:46:18 +0300

    refactor: update

--- streaming.entry.js
+++ streaming.entry.js
@@ -2,16 +2,15 @@ const { init } = require('@app/omni/init');
 init();
 const { startSocket } = require('@sotaoi/api/io');
 const fs = require('fs');
-const path = require('path');
 const { getAppInfo } = require('@sotaoi/omni/get-app-info');
 
 const main = async () => {
   const appInfo = getAppInfo();
   startSocket(
     appInfo.streamingPort,
-    fs.readFileSync(path.resolve(appInfo.sslKey)),
-    fs.readFileSync(path.resolve(appInfo.sslCert)),
-    fs.readFileSync(path.resolve(appInfo.sslCa)),
+    fs.readFileSync(require.resolve(appInfo.sslKey)),
+    fs.readFileSync(require.resolve(appInfo.sslCert)),
+    fs.readFileSync(require.resolve(appInfo.sslCa)),
   );
 };
 
